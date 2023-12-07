5e78c0feed941bcabf866f563ca9fced8988447a
mfunyu <mfunyu@student.42lyon.fr>
2023-12-07 15:07:27 +0100

    update: add protection try-catch

--- ft_otp.py
+++ ft_otp.py
@@ -33,11 +33,12 @@ def create_qrcode(secret_key):
 	data = f'otpauth://totp/{issuer}:{email}?secret={secret_key}&issuer={issuer}'
 	img = qrcode.make(data)
 	img.save("ft_opt.png")
+	print("QRcode can be found in ft_otp.png")
 
 def generate_shared_secret_key(key_hex_file):
 	try:
 		with open(key_hex_file, 'r') as f:
-			contents = f.read()
+			contents = f.read().strip()
 			if not is_hex_string(contents):
 				return
 		hashed = hashlib.sha1(contents.encode())
@@ -46,7 +47,6 @@ def generate_shared_secret_key(key_hex_file):
 			f.write(encoded)
 		print("Key was successfully saved in ft_otp.key")
 		create_qrcode(encoded)
-		print("QRcode can be found in ft_otp.png")
 
 	except Exception as e:
 		print(f"Error: {e}")
@@ -67,27 +67,32 @@ def debug_print(hmac_sha1, offset, chosen_bytes):
 
 def generate_otp(key_file):
 
-	with open(key_file, 'r') as f:
-		secret = f.read()
-	secret_decoded = base64.b32decode(secret)
-	timestamp = time.time()
-	N = int(timestamp // time_step)
-	time_key = N.to_bytes(8, "big")
+	try:
+		with open(key_file, 'r') as f:
+			secret = f.read()
+		secret_decoded = base64.b32decode(secret)
 
-	hmac_sha1 = hmac.new(secret_decoded, time_key, hashlib.sha1)
-	hmac_sha1_bytes = hmac_sha1.digest()
-	offset = hmac_sha1_bytes[-1] & 0xF
-	chosen_bytes = hmac_sha1_bytes[offset:offset+4]
+		timestamp = time.time()
+		N = int(timestamp // time_step)
+		time_key = N.to_bytes(8, "big")
 
-	#debug_print(hmac_sha1, offset, chosen_bytes)
+		hmac_sha1 = hmac.new(secret_decoded, time_key, hashlib.sha1)
+		hmac_sha1_bytes = hmac_sha1.digest()
+		offset = hmac_sha1_bytes[-1] & 0xF
+		chosen_bytes = hmac_sha1_bytes[offset:offset+4]
 
-	new_bin_value = ((chosen_bytes[0] & 0x7F) << 24)\
-					+ ((chosen_bytes[1] & 0xFF) << 16)\
-					+ ((chosen_bytes[2] & 0xFF) << 8)\
-					+ ((chosen_bytes[3] & 0xFF))
-	token = new_bin_value % 10**6
-	token = f'{token:0>6}'
-	print(f"Here is your OTP: [{token}]")
+		#debug_print(hmac_sha1, offset, chosen_bytes)
+
+		new_bin_value = ((chosen_bytes[0] & 0x7F) << 24)\
+						+ ((chosen_bytes[1] & 0xFF) << 16)\
+						+ ((chosen_bytes[2] & 0xFF) << 8)\
+						+ ((chosen_bytes[3] & 0xFF))
+		token = new_bin_value % 10**6
+		token = f'{token:0>6}'
+		print(f"Here is your OTP: [{token}]")
+
+	except Exception as e:
+		print(f"Error: {e}")
 
 def parse_args():
 	parser = argparse.ArgumentParser()
